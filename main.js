ab8b6a05884841aa988ca5ca5394361c0237062e
joaodelas6901 <161736695+joaodelas6901@users.noreply.github.com>
2024-05-16 10:38:26 -0300

    llllllllll

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2025-05-05T00:00:00");
+const tempoObjetivo1 = new Date("2026-05-05T00:00:00");
 const tempoObjetivo2 = new Date("2025-03-04T00:00:00");
 const tempoObjetivo3 = new Date("2030-12-05T00:00:00");
 const tempoObjetivo4 = new Date("2026-04-10T00:00:00");
